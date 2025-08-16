697317b7a10c5dfb6b61fb276eee31c31837736a
Yasin <yasin.enis.tr@gmail.com>
2025-08-16 22:39:08 +0300

    ✨EJS added and routed pages

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ import express from 'express';
 
 const app = express();
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// ROUTES
 app.get('/', (req, res) => {
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`Server started at ${port} port..`);
 });
