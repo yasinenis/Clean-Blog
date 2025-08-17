4478770eab944b922d62dff7d1446ca8aa900438
Yasin <yasin.enis.tr@gmail.com>
2025-08-17 20:38:26 +0300

    ✨addPost functionality added

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express';
 
+import Post from './models/Post.js';
+
+import mongoose from 'mongoose';
+
 const app = express();
+
+// connect DB
+await mongoose.connect('mongodb://localhost/cleanblog-test-db');
+console.log('Database connected succesfully.');
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -8,9 +16,13 @@ app.set('view engine', 'ejs');
 // MIDDLEWARES
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true })); // helps us to read data in url
+app.use(express.json()); // converts url data to json
+
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', { posts });
 });
 
 app.get('/about', (req, res) => {
@@ -19,6 +31,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
---
+++ models/Post.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+// create schema
+const PostSchema = new Schema({
+  title: String,
+  detail: String,
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+export default Post;
