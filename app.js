56649e44e1a5a9c7ba8e23d8e76b0584e6954d3b
Yasin <yasin.enis.tr@gmail.com>
2025-08-21 12:11:33 +0300

    🌿ready to deploy

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ import * as pageControllers from './controllers/pageControllers.js';
 const app = express();
 
 // connect DB
-await mongoose.connect('mongodb://localhost/cleanblog-test-db');
-console.log('Database connected succesfully.');
+await mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Database connected succesfully!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -39,7 +45,7 @@ app.get('/about', pageControllers.getAboutPage);
 app.get('/add_post', pageControllers.getAddPage);
 app.get('/posts/edit/:id', pageControllers.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started at ${port} port..`);
 });
